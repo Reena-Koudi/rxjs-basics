f15c47ef870ee6e6b2d6f98bb20a4c7560d8e998
Reena Koudi <reena.koudi@holition.com>
2020-01-20 13:36:27 +0000

    setting up the node server & setting up google map API and darksky API accounts

--- server/server.js
+++ server/server.js
@@ -1,35 +1,30 @@
-const express = require("express");
+const express = require('express');
 const fetch = require('node-fetch');
-var cors = require("cors");
+var cors = require('cors');
 const app = express();
 const port = 3000;
 app.use(cors());
 
-
 // Add your keys
 const keys = {
   //https://developers.google.com/places/web-service/intro
-  googleMaps : "YOUR KEY GOES HERE",
+  googleMaps: 'AIzaSyAq9WMellCS0lAfkliFydoc3kI6wGOOOBw',
   //https://darksky.net/dev
-  darkSky : "YOUR KEY GOES HERE"
+  darkSky: '446bd447c983e182b7acbf45d916a329'
 };
 
-
-const googleMapsClient = require("@google/maps").createClient({
+const googleMapsClient = require('@google/maps').createClient({
   key: keys.googleMaps
 });
-const util = require("@google/maps").util;
+const util = require('@google/maps').util;
 
 const token = util.placesAutoCompleteSessionToken();
 
-
-app.get("/autocomplete/:id", (req, res) => {
-  
-
+app.get('/autocomplete/:id', (req, res) => {
   googleMapsClient.placesAutoComplete(
     {
       input: req.params.id,
-      language: "en",
+      language: 'en',
       sessiontoken: token
     },
     (_, result) => {
@@ -38,28 +33,26 @@ app.get("/autocomplete/:id", (req, res) => {
   );
 });
 
-app.get("/place/:id", (req, res) => {
+app.get('/place/:id', (req, res) => {
   googleMapsClient.place(
     {
-        placeid: req.params.id,
-        language: 'en'
+      placeid: req.params.id,
+      language: 'en'
     },
     (_, result) => {
-        
       res.send(result.json.result);
     }
   );
 });
 
-
-
-app.get("/weather/:lat/:long", (req, res) => {
-    fetch(`https://api.darksky.net/forecast/${keys.darkSky}/${req.params.lat},${req.params.long}`)
+app.get('/weather/:lat/:long', (req, res) => {
+  fetch(
+    `https://api.darksky.net/forecast/${keys.darkSky}/${req.params.lat},${req.params.long}`
+  )
     .then(res => res.json())
     .then(json => res.json(json));
-
-
 });
 
-
-app.listen(port, () => console.log(`The Weather Server is running on ${port}!`));
+app.listen(port, () =>
+  console.log(`The Weather Server is running on ${port}!`)
+);
