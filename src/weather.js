5c1d85ed202716afee35aca84a03a40bfe7c3741
Reena Koudi <reena.koudi@holition.com>
2020-01-20 15:26:08 +0000

    get place data

--- src/weather.js
+++ src/weather.js
@@ -5,6 +5,7 @@ import {} from './helpers';
 // Handles to our Elements
 const searchBox = document.getElementById('search');
 const resultsBox = document.getElementById('results-container');
+const spinner = document.getElementById('spinner');
 
 // Event Handlers
 const searchEvent = fromEvent(searchBox, 'keyup');
@@ -14,3 +15,31 @@ const resultsEvent = fromEvent(resultsBox, 'click');
 const inputSubject = new BehaviorSubject('');
 const placeSubject = new Subject();
 const weatherSubject = new Subject();
+
+const inputData = inputSubject
+  .pipe(
+    skip(1),
+    tap(() => {
+      spinner.className = 'spinner';
+    }),
+    debounceTime(1000),
+    switchMap(searchTerm => {
+      return ajax
+        .getJSON(`http://localhost:3000/autocomplete/${searchTerm}`)
+        .pipe(
+          tap(() => {
+            spinner.className = '';
+          }),
+          switchMap(results => {
+            return from(results);
+          })
+        );
+    })
+  )
+  .subscribe(result => {
+    add.result(result.description, result.place_id);
+  });
+
+searchEvent.subscribe(ev => {
+  inputSubject.next(searchBox.value);
+});
